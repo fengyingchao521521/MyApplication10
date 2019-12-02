f5d083684848a91fc81006b163c275a6351c1cf4
fengyingchao <email@example.cpm>
2019-12-02 13:50:28 +0800

    封英超第一周考试代码

---
+++ app/src/main/java/com/bw/myapplication/view/MainActivity.java
@@ -0,0 +1,16 @@
+package com.bw.myapplication.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.bw.myapplication.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
